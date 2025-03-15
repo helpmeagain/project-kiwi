6999b5c8c7b1ae877d9c34ff33b359b234112309
helpmeagain <felipe.comarques1208@gmail.com>
2025-03-15 17:17:47 -0300

    feat: add multiplayer menu

---
+++ src/scripts/multiplayer_controller.gd
@@ -0,0 +1,12 @@
+extends Control
+
+@export var address = "127.0.0.1"
+
+func _on_host_button_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_join_button_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_start_button_pressed() -> void:
+	pass # Replace with function body.
