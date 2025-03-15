8149b9e2b1f03e6c67ca6689d2bcdc773859c012
helpmeagain <felipe.comarques1208@gmail.com>
2025-03-15 18:34:43 -0300

    feat: add multiplayer with host and clients

--- src/scripts/main_menu.gd
+++ src/scripts/main_menu.gd
@@ -1,4 +1,4 @@
 extends Control
 
 func _on_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://src/scenes/questions.tscn")
+	get_tree().change_scene_to_file("res://src/scenes/multiplayer-menu.tscn")
--- src/scripts/multiplayer_controller.gd
+++ src/scripts/multiplayer_controller.gd
@@ -1,12 +1,53 @@
 extends Control
 
 @export var address = "127.0.0.1"
+@export var port = 8910
+var peer
+
+func _ready() -> void:
+	multiplayer.peer_connected.connect(player_connected)
+	multiplayer.peer_disconnected.connect(player_disconnected)
+	multiplayer.connected_to_server.connect(player_connected_to_server)
+	multiplayer.connection_failed.connect(player_connected)
+
+func player_connected(id):
+	print("Player connected: " + str(id))
+
+func player_disconnected(id):
+	print("Player disconnected: " + str(id))
+	
+func player_connected_to_server():
+	print("Connected to server")
+	
+func player_connection_failed():
+	print("Connection failed")
+
+@rpc("any_peer", "call_local")
+func start_game():
+	var scene = load("res://src/scenes/questions.tscn").instantiate()
+	get_tree().root.add_child(scene)
+	self.hide()
 
 func _on_host_button_pressed() -> void:
-	pass # Replace with function body.
+	peer = ENetMultiplayerPeer.new()
+	var error = peer.create_server(port, 45)
+	if error != OK:
+		print("Cannot host: " + error)
+		return
+	# peer.get_host().compress(ENetConnection.COMPRESS_RANGE_CODER)
+	
+	multiplayer.set_multiplayer_peer(peer)
+	print("Waiting for players...")
 
 func _on_join_button_pressed() -> void:
-	pass # Replace with function body.
+	peer = ENetMultiplayerPeer.new()
+	peer.create_client(address, port)
+	#peer.get_host().compress(ENetConnection.COMPRESS_RANGE_CODER)
+	multiplayer.set_multiplayer_peer(peer)
+	print("Joined game!")
 
 func _on_start_button_pressed() -> void:
-	pass # Replace with function body.
+	if multiplayer.is_server():
+		start_game.rpc()
+	else:
+		print("Only host can start the game")
