72d6ff96d84440182716068a50fe710c926e60f5
helpmeagain <felipe.comarques1208@gmail.com>
2025-03-29 14:00:18 -0300

    feat: improve 'fill in the blank' user interface

--- src/scripts/exercises/fill_in_the_blank.gd
+++ src/scripts/exercises/fill_in_the_blank.gd
@@ -26,7 +26,7 @@ func setup_buttons() -> void:
 	options.shuffle()
 	
 	for i in 4:
-		var button = get_node("Button" + str(i + 1))
+		var button = $VBoxContainer.get_child(floor(i / 2)).get_child(i % 2)
 		button.text = options[i]
 
 func _on_button_pressed(button: Button) -> void:
@@ -36,13 +36,13 @@ func _on_button_pressed(button: Button) -> void:
 		emit_signal("answer_wrong")
 
 func _on_button_1_pressed() -> void:
-	_on_button_pressed($Button1)
+	_on_button_pressed($VBoxContainer/HBoxContainer/Button1)
 
 func _on_button_2_pressed() -> void:
-	_on_button_pressed($Button2)
+	_on_button_pressed($VBoxContainer/HBoxContainer/Button2)
 
 func _on_button_3_pressed() -> void:
-	_on_button_pressed($Button3)
+	_on_button_pressed($VBoxContainer/HBoxContainer2/Button3)
 
 func _on_button_4_pressed() -> void:
-	_on_button_pressed($Button4)
+	_on_button_pressed($VBoxContainer/HBoxContainer2/Button4)
