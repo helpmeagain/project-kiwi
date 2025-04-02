58fca768b11e32d57e21a7b125b922fe0391ac3b
Felipe Marques <felipe.comarques1208@gmail.com>
2025-04-02 11:42:25 -0300

    feat: add about menu

---
+++ src/scripts/menus/about_menu.gd
@@ -0,0 +1,40 @@
+extends Control
+
+func _on_home_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://src/scenes/menus/main-menu.tscn")
+
+func _on_tengu_button_pressed() -> void:
+	OS.shell_open("https://picrew.me/en/image_maker/1979868")
+
+func _on_silver_button_pressed() -> void:
+	OS.shell_open("https://poppyworks.itch.io/silver")
+
+func _on_abbadon_button_pressed() -> void:
+	OS.shell_open("https://caffinate.itch.io/abaddon")
+
+func _on_edit_undo_button_pressed() -> void:
+	OS.shell_open("https://www.dafont.com/edit-undo.font")
+
+func _on_ruler_button_pressed() -> void:
+	OS.shell_open("https://somepx.itch.io/free-font-ruler")
+
+func _on_dialog_sound_button_pressed() -> void:
+	OS.shell_open("https://alan-dalcastagne.itch.io/dialog-text-sound-effects")
+
+func _on_gui_elements_button_pressed() -> void:
+	OS.shell_open("https://mounirtohami.itch.io/pixel-art-gui-elements")
+
+func _on_ui_assets_pack_button_pressed() -> void:
+	OS.shell_open("https://srtoasty.itch.io/ui-assets-pack-2")
+
+func _on_ui_cozy_button_pressed() -> void:
+	OS.shell_open("https://toffeecraft.itch.io/ui-user-interface-pack-cozy-coffee")
+
+func _on_lucid_button_pressed() -> void:
+	OS.shell_open("https://leo-red.itch.io/lucid-icon-pack")
+
+func _on_git_hub_button_pressed() -> void:
+	OS.shell_open("https://github.com/helpmeagain")
+
+func _on_repository_button_pressed() -> void:
+	OS.shell_open("https://github.com/helpmeagain/project-kiwi")
--- src/scripts/menus/main_menu.gd
+++ src/scripts/menus/main_menu.gd
@@ -2,9 +2,15 @@ extends Control
 
 func _on_singleplayer_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://src/scenes/gameplay/visual-novel.tscn")
-	
-func _on_button_pressed() -> void:
+
+func _on_multiplayer_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://src/scenes/menus/multiplayer-menu.tscn")
+
+func _on_about_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://src/scenes/menus/about_menu.tscn")
+
+func _on_how_to_play_button_pressed() -> void:
+	pass
 
 func _on_exit_button_pressed() -> void:
 	get_tree().quit()
