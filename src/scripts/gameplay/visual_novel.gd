363ad303b2b5edb874cc901bd1e33796976ee7b3
helpmeagain <felipe.comarques1208@gmail.com>
2025-04-05 16:21:39 -0300

    feat: add background transition

--- src/scripts/gameplay/visual_novel.gd
+++ src/scripts/gameplay/visual_novel.gd
@@ -7,9 +7,14 @@ var exercise_config: Dictionary
 func _ready() -> void:
 	exercise_config = load_exercise_config()
 	Dialogic.signal_event.connect(_on_dialogic_signal)
-	start_dialogic_timeline("school-entrance")
+	$StartGameControl.show()
 
 func _on_dialogic_signal(argument: String):
+	if argument.begins_with("bg_"):
+		var parts = argument.split(",")
+		var bg_name = parts[0].trim_prefix("bg_")
+		var transition = parts[1] if parts.size() > 1 else "fade"
+		set_background(bg_name, transition)
 	if exercise_config.has(argument):
 		setup_exercise(argument)
 
@@ -23,7 +28,24 @@ func setup_exercise(exercise_key: String) -> void:
 		current_exercise.initialize(config.data.file, config.data.id)
 	
 	_connect_exercise_signals(current_exercise)
-	$QuestionsContainer.add_child(current_exercise)
+	$QuestionsControl.add_child(current_exercise)
+
+func set_background(background_name: String, transition_type: String = "fade") -> void:
+	var extensions = [".png", ".jpg", ".jpeg"]
+	var bg_path := ""
+	var new_texture: Texture2D = null
+	
+	for ext in extensions:
+		var try_path = "res://src/assets/backgrounds/%s%s" % [background_name, ext]
+		if ResourceLoader.exists(try_path):
+			bg_path = try_path
+			new_texture = load(bg_path)
+			break
+	
+	if new_texture:
+		$BackgroundControl/ImageBackground.change_background(new_texture, transition_type)
+	else:
+		push_error("Background not found with supported extensions for: " + background_name)
 	
 func load_exercise_config() -> Dictionary:
 	var config_file = FileAccess.open("res://src/assets/exercises/story-exercises-config.json", FileAccess.READ)
@@ -49,13 +71,11 @@ func _on_answer_correct():
 
 func _on_answer_wrong():
 	Dialogic.VAR.wrong_answer += 1
-	$QuestionsContainer.mouse_filter = Control.MOUSE_FILTER_IGNORE
-	$BlackScreen.show()
-	$WrongPanel.show()
+	$QuestionsControl.mouse_filter = Control.MOUSE_FILTER_IGNORE
+	$WrongAnswer.show()
 
 func _cleanup_exercise():
-	if current_exercise:
-		$QuestionsContainer.remove_child(current_exercise)
+		$QuestionsControl.remove_child(current_exercise)
 		current_exercise.queue_free()
 		current_exercise = null
 		current_next_timeline = ""
@@ -64,6 +84,9 @@ func start_dialogic_timeline(timeline: String) -> void:
 	Dialogic.start(timeline)
 
 func _on_wrong_panel_button_pressed() -> void:
-	$QuestionsContainer.mouse_filter = Control.MOUSE_FILTER_STOP
-	$BlackScreen.hide()
-	$WrongPanel.hide()
+	$WrongAnswer.hide()
+	$QuestionsControl.mouse_filter = Control.MOUSE_FILTER_STOP
+
+func _on_start_game_button_pressed() -> void:
+	$StartGameControl.hide()
+	start_dialogic_timeline("school-entrance")
