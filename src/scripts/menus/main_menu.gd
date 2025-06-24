662925a9fc9ac5b1047cea620ff38c0e0644f83b
Felipe Marques <felipe.comarques1208@gmail.com>
2025-06-24 14:01:40 -0300

    feat: add singleplayer menu

--- src/scripts/menus/main_menu.gd
+++ src/scripts/menus/main_menu.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _on_singleplayer_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://src/scenes/gameplay/visual-novel.tscn")
+	get_tree().change_scene_to_file("res://src/scenes/menus/singleplayer-menu.tscn")
 
 func _on_multiplayer_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://src/scenes/menus/multiplayer-menu.tscn")
@@ -9,8 +9,6 @@ func _on_multiplayer_button_pressed() -> void:
 func _on_about_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://src/scenes/menus/about-menu.tscn")
 
-func _on_how_to_play_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://src/scenes/gameplay/arcade-questions.tscn")
 
 func _on_exit_button_pressed() -> void:
 	get_tree().quit()
---
+++ src/scripts/menus/singleplayer_menu.gd
@@ -0,0 +1,10 @@
+extends Control
+
+func _on_back_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://src/scenes/menus/main-menu.tscn")
+
+func _on_history_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://src/scenes/gameplay/visual-novel.tscn")
+
+func _on_arcade_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://src/scenes/gameplay/arcade-questions.tscn")
